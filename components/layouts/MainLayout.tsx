f4f8f6ac6f8752a9e8542f9a646bc7056cd8e153
Nicolas Castaneda <vladistar200304@gmail.com>
2023-03-22 17:41:42 -0500

    Pre-deployment Vercel

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,33 @@
+import { FC, ReactNode } from 'react';
+
+import Head from 'next/head';
+
+import { Navbar } from '../Navbar';
+
+
+import styles from './MainLayout.module.css';
+
+type Props = {
+  children: ReactNode;
+}
+
+export const MainLayout: FC<Props> = ({ children }) => {
+    return (
+        <div className={styles.container}>
+          
+          <Head>
+            <title>Home - Nicolas</title>
+            <meta name="description" content="Home Page" />
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+        
+          <Navbar />
+    
+          <main className={styles.main}>
+    
+            { children }
+    
+          </main>     
+        </div>
+      )
+};
